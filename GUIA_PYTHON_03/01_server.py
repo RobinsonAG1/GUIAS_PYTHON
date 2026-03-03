c6f1ec216a179920b594360942e7a37b29c79492
Robinson Ramiro Aguilar Velasco <aguilarr650@gmail.com>
2026-03-03 18:54:56 -0500

    guia 03
    guia 03 python

---
+++ GUIA_PYTHON_03/01_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', methods=['GET']) 
+def home() -> str:
+    return "<h1>Sistema de Control Agroindustrial Activo</h1>"
+
+if __name__ == "__main__":
+    print("Iniciando servidor TCP [http://127.0.0.1.:5000](http://127.0.0.1:5000)")
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+#http://localhost:5000
---
+++ GUIA_PYTHON_03/Ejercicio_practico_01.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/temperatura/<float:grados_c>', methods=['GET'])
+def convertir(grados_c: float) -> str:
+    fahrenheit: float = (grados_c * 9/5) + 32
+    return f"{grados_c}°C a {fahrenheit}°F"
+
+if __name__ == "__main__":
+    app.run(debug=True)
